b6dbdf46000c77b407ac749561425f8627ac36ac
MilosPaunovic <paun992@hotmail.com>
2024-11-28 12:52:58 +0100

    chore(ui): rmeove unnecesary console statement

--- ui/src/components/dashboard/Dashboard.vue
+++ ui/src/components/dashboard/Dashboard.vue
@@ -305,7 +305,6 @@
         if (route.name === "home") {
             router.replace({params: {...route.params, id: v?.id ?? "default"}});
             if (v && v.id !== "default") {
-                console.log(route)
                 dashboard = await store.dispatch("dashboard/load", v.id);
             }
 
